359d1ee8c00f5b8a86656cd7c440a78db2fdd4a6
Sho Neriki <ikirenohs@gmail.com>
2023-06-06 12:22:10 +0900

    hamburger menu initial code

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const navLinks = document.getElementById("nav-links");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navList = document.querySelectorAll('.nav-links li');
+
+function toggleNav() {
+  navLinks.classList.toggle("nav-active");
+}
+
+hamburgerMenu.addEventListener("click", toggleNav);
+
+navList.forEach(link => {
+  link.addEventListener('click', toggleNav);
+});
